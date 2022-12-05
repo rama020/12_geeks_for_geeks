f9b77027d7c3b548eaacaea1583582a7accaba1d
rama020 <118764128+rama020@users.noreply.github.com>
2022-12-05 16:45:19 +0530

    Add files via upload

---
+++ strings 0.py
@@ -0,0 +1,25 @@
+# Python Program for
+# Creation of String
+
+# Creating a String
+# with single Quotes
+k='this is pooja'
+print("String with the use of Single Quotes: ")
+print(k)
+
+# Creating a String
+# with double Quotes
+b = "I'm a student"
+print(b)
+
+# Creating a String
+# with triple Quotes
+c = '''i am rama'''
+print(c)
+
+# Creating String with triple
+# Quotes allows multiple lines
+d = '''Geeks
+			For
+			Life'''
+print(d)
