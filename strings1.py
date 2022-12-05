3b73d18802b6afd2381037005842370c8141df1a
rama020 <118764128+rama020@users.noreply.github.com>
2022-12-05 16:46:05 +0530

    Add files via upload

---
+++ strings1.py
@@ -0,0 +1,13 @@
+# python Program to Access
+# characters of String
+
+a = "GeeksForGeeks"
+print(a)
+
+# Printing First character
+print("\nFirst character of String is: ")
+print(a[0])
+
+# Printing Last character
+print("\nLast character of String is: ")
+print(a[-1])
